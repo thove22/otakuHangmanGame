6110293c4ef04c4fc8e292c3f8b8a6e9c6c234bd
thove22 <pedrostovela@gmail.com>
2025-09-26 18:52:30 +0200

    fix: Remove src submodule and add Java source files properly

---
+++ src/main/com/otakuhangman/Game.java
@@ -0,0 +1,54 @@
+package main.com.otakuhangman;
+import java.io.*;
+
+public class Game {
+
+    public  void start(){
+        int userChoice  = displayMenu();
+        handleMenuChoice(userChoice);
+    }
+
+    private int displayMenu() {
+        try (BufferedReader br =  new BufferedReader(new InputStreamReader(System.in))){
+            int choice = 0;
+            do{
+                System.out.println("≡≡≡ O T A K U - H A N G M A N ≡≡≡");
+                System.out.println("1.Novo Jogo");
+                System.out.println("2.Continuar");
+                System.out.println("3.Como Jogar");
+                System.out.println("4.Sair");
+                System.out.print("Escolha uma opcao: ");
+                try{
+                    choice = Integer.parseInt(br.readLine());
+                    if (choice < 1 || choice > 4){
+                        System.out.println("Opcao Invalida!!!");
+                    }
+                }catch (NumberFormatException e){
+                    System.out.println("Entrada Invalida! Por favor, digite apenas numeros.");
+                    choice = 0;
+                }
+            }while (choice < 1 || choice > 4);
+
+            return choice;
+
+        }catch (IOException e){
+            System.out.println("Erro de Leitura");
+            exitGame();
+            return  0;
+        }
+    }
+
+    private  void handleMenuChoice(int userChoice){
+        switch (userChoice){
+            case 1: exitGame();
+                   break;
+            default:
+                    System.out.println("Opcao Invalida!!!");
+        }
+    }
+
+    private void exitGame(){
+        System.out.println("Obriagdo por Jogar o Otaku HangMan, Sayounara!!!!");
+        System.exit(0);
+    }
+}
---
+++ src/main/com/otakuhangman/Main.java
@@ -0,0 +1,8 @@
+package main.com.otakuhangman;
+
+public class Main {
+    public static void main(String [] args){
+        Game game = new Game();
+        game.start();
+    }
+}
