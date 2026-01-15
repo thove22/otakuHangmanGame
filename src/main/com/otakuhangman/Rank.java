aa55d02ef11b2e073e041bfbdff0c914f82de6dd
thove22 <pedrostovela@gmail.com>
2026-01-15 16:56:12 +0200

    feat: Implement calculateBaseScore method, that will return the score based on the number of errors

--- src/main/com/otakuhangman/Rank.java
+++ src/main/com/otakuhangman/Rank.java
@@ -1,8 +1,8 @@
 package main.com.otakuhangman;
 
 public enum Rank {
-    INICIANTE,
-    NUTELLA,
+    OTAKU_INICIANTE,
+    OTAKU_NUTELLA,
     MID_OTAKU,
     ADVANCED_OTAKU,
     GOD_OTAKU,
--- src/main/com/otakuhangman/ScoreCalculator.java
+++ src/main/com/otakuhangman/ScoreCalculator.java
@@ -1,5 +1,21 @@
 package main.com.otakuhangman;
 
 public class ScoreCalculator {
-    private Rank rank;
+
+    public static int calculateBaseScore(int errors){
+
+        if(errors < 0 || errors > 6 ){
+            throw new IllegalArgumentException(
+                    "Numero de Erros Invalidos: " + errors + ". O valor deve estar entre 0 e 6"
+            );
+        }
+
+        for (ScoreBase scoreBase : ScoreBase.values()){
+            if (scoreBase.getErrors() == errors){
+                return scoreBase.getScore();
+            }
+        }
+
+        throw new IllegalStateException("Score nao Encontrado para o Erro");
+    }
 }
