8a69f9ec65904ba716c3e65aa669679919e88269
thove22 <pedrostovela@gmail.com>
2026-01-16 02:51:43 +0200

    feat: update Rank Enum with Required Points to each Rank

--- src/main/com/otakuhangman/Rank.java
+++ src/main/com/otakuhangman/Rank.java
@@ -1,9 +1,19 @@
 package main.com.otakuhangman;
 
 public enum Rank {
-    OTAKU_INICIANTE,
-    OTAKU_NUTELLA,
-    MID_OTAKU,
-    ADVANCED_OTAKU,
-    GOD_OTAKU,
+    OTAKU_INICIANTE(0),
+    OTAKU_NUTELLA(1000),
+    MID_OTAKU(3000),
+    ADVANCED_OTAKU(5000),
+    GOD_OTAKU(9500);
+
+    private final int requiredPoints;
+
+    Rank(int requiredPoints){
+        this.requiredPoints = requiredPoints;
+    }
+
+    public int getRequiredPoints() {
+        return requiredPoints;
+    }
 }
