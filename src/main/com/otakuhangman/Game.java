698e511649a6842adca8502b1fe24c1abfd0c704
thove22 <pedrostovela@gmail.com>
2026-01-23 14:12:03 +0200

    feat: implement playCurrentLevel method

--- src/main/com/otakuhangman/Game.java
+++ src/main/com/otakuhangman/Game.java
@@ -1,9 +1,10 @@
 package main.com.otakuhangman;
 import java.io.*;
+import java.nio.charset.Charset;
 import java.util.List;
+import java.util.Set;
 
 public class Game {
-
     private Player currentPlayer;
     private List<Level> levels;
     private int currentLevelIndex;
@@ -78,7 +79,7 @@ public class Game {
             String name = getValidPlayerName(br);
             currentPlayer = new Player(name);
             currentLevelIndex = 0;
-            //playCurrentLevel
+            playCurrentLevel();
         } catch (IOException e) {
             System.out.println("Erro de Entrada: " + e.getMessage());
         }
@@ -100,6 +101,65 @@ public class Game {
                 continue;
             }
             return name;
+        }
+    }
+
+    void playCurrentLevel(){
+        try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            currentPlayer.setCurrentLevelScore(0);
+            Level currentLevel = levels.get(currentLevelIndex);
+            List<Challenge> challenges = currentLevel.getChallenges();
+            int totalChallenges = challenges.size();
+            for (int i = 0 ; i < totalChallenges ; i++){
+                Challenge challenge = challenges.get(i);
+                System.out.println("Desafio " + (i + 1) + " de " + totalChallenges);
+                System.out.println(challenge.getHint());
+                System.out.println(challenge.getStatus());
+
+                while (!challenge.isComplete()){
+                    char letter = getTriedLetter(br);
+                    boolean correct =  challenge.tryLetter(letter);
+                    System.out.println(correct ? "Tentativa Correcta" : "Tentativa Incorecta" );
+
+                    if (!correct){
+                        //drawHangMan(challenge.getCurrentErrors());
+                    }
+                }
+                System.out.println(challenge.isWon() ? "\nPARABÉNS! Você adivinhou: " + challenge.getWord()
+                        : "\n💀 FORCA COMPLETA! A palavra era: " + challenge.getWord());
+                int errors = challenge.getCurrentErrors();
+                int pointEarned = currentPlayer.processChallengesResult(errors);
+
+                System.out.println(currentPlayer.getPlayerStatus());
+                System.out.println("\n Resultados do Jogador: ");
+                System.out.println("Pontos: " + pointEarned);
+                System.out.println("\nPressione Enter para continuar...");
+                br.readLine();
+            }
+            currentPlayer.advanceToNextLevel();
+        }catch (IOException e){
+            System.out.println(e.getMessage());
+        }
+    }
+    char getTriedLetter(BufferedReader br) throws IOException{
+        while (true){
+            System.out.println("Insira uma letra: ");
+            String input = br.readLine();
+            if (input.isEmpty()){
+                System.out.println("Entrada vazia. Insira uma letra: .");
+                continue;
+            }
+            if (input.length() != 1){
+                System.out.println("Insira apenas uma letra.");
+            }
+            char letter =  input.charAt(0);
+
+            if (!Character.isLetter(letter) && !Character.isDigit(letter)){
+                System.out.println("Use apenas letras ou numeros");
+                continue;
+            }
+            return letter;
         }
     }
     void exitGame(){
