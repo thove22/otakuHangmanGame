a768070f21858d8b2ddd709d1cf634d51f224e35
thove22 <pedrostovela@gmail.com>
2026-01-29 11:11:46 +0200

    feat: implement game gui interface

--- src/main/com/otakuhangman/gui/GameGui.java
+++ src/main/com/otakuhangman/gui/GameGui.java
@@ -1,4 +1,14 @@
 package main.com.otakuhangman.gui;
 
-public class GameGui {
+import main.com.otakuhangman.core.AttemptResult;
+import main.com.otakuhangman.core.Challenge;
+import main.com.otakuhangman.core.Level;
+import main.com.otakuhangman.core.Player;
+
+public interface GameGui {
+    void showMenu();
+    void showChallenge(Challenge challenge);
+    void showAttempResult(AttemptResult result);
+    void showLevelResult(Player player, Level level);
+    void showGameOver(Player player);
 }
