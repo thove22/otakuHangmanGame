269239aba6fbf7b0fc8510f6c4a6b36879c9c383
thove22 <pedrostovela@gmail.com>
2026-01-31 12:02:59 +0200

    feat: add background motion dots on Menu Screen

--- src/main/com/otakuhangman/gui/screens/MenuScreen.java
+++ src/main/com/otakuhangman/gui/screens/MenuScreen.java
@@ -1,21 +1,29 @@
 package main.com.otakuhangman.gui.screens;
-
 import main.com.otakuhangman.gui.Screen;
-
+import main.com.otakuhangman.gui.utils.Star;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.util.Random;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MenuScreen extends Screen {
     private final Color HOVER_COLOR = new  Color(70, 100, 255);
     private final Color DEFAULT_COLOR = Color.WHITE;
-    private Point[] stars;
+    private List<Star> starField;
+    private Timer animationTimer;
+    private final int STAR_COUNT = 150;
+
     public MenuScreen() {
         setLayout(new GridBagLayout());
         setBackground(Color.BLACK);
-        generateStars(100);
+
+        starField = new ArrayList<>();
+        for (int i = 0; i < STAR_COUNT; i++) {
+            starField.add(new Star(1200, 800));
+        }
+
         JPanel menuPanel = new JPanel();
         menuPanel.setLayout(new BoxLayout(menuPanel, BoxLayout.Y_AXIS));
         menuPanel.setOpaque(false);
@@ -27,10 +35,16 @@ public class MenuScreen extends Screen {
         menuPanel.add(Box.createRigidArea(new Dimension(0, 20)));
         menuPanel.add(createMenuItem("QUIT", 4));
         add(menuPanel, new GridBagConstraints());
+        animationTimer = new Timer(16, e -> {
+            for (Star star : starField) {
+                star.update(getWidth(), getHeight());
+            }
+            repaint();
+        });
     }
     private JLabel createMenuItem(String text, int choice){
         JLabel item = new JLabel(text);
-        item.setFont(new Font("Monospaced", Font.BOLD, 28));
+        item.setFont(new Font("Monospaced", Font.BOLD, 40));
         item.setForeground(DEFAULT_COLOR);
         item.setAlignmentX(Component.CENTER_ALIGNMENT);
         item.setCursor(new Cursor(Cursor.HAND_CURSOR));
@@ -57,24 +71,22 @@ public class MenuScreen extends Screen {
             default -> System.out.println("Option " + choice + " selected.");
         }
     }
-    private void generateStars(int count) {
-        Random rand = new Random();
-        stars = new Point[count];
-        for (int i = 0; i < count; i++) {
-            stars[i] = new Point(rand.nextInt(1200), rand.nextInt(800));
-        }
-    }
+
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-        g.setColor(Color.WHITE);
-        for (Point star : stars) {
-            g.fillRect(star.x, star.y, 2, 2);
+        Graphics2D g2d = (Graphics2D) g;
+        g2d.setColor(new Color(200, 200, 200, 150));
+        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        for (Star star : starField) {
+            g2d.fillOval((int)star.x, (int)star.y, star.size, star.size);
         }
     }
 
     public void onEnter() {
         this.requestFocusInWindow();
+        animationTimer.start();
     }
     public void onExit() {
+        animationTimer.stop();
     }
 }
